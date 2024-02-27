d3f6c0e9963e234aa14fff6b91e668bf06af2fd5
Eashaque Rahman Sami <46522742+eashaksami@users.noreply.github.com>
2024-02-27 14:50:28 +0600

    Add files via upload
    Initial Template done

---
+++ url-shortener/src/main/java/com/example/urlshortener/controller/UrlController.java
@@ -0,0 +1,35 @@
+package com.example.urlshortener.controller;
+
+
+import com.example.urlshortener.models.UrlLongRequest;
+import com.example.urlshortener.service.UrlService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UrlController {
+
+    private final UrlService urlService;
+
+    public UrlController(UrlService urlService) {
+        this.urlService = urlService;
+    }
+
+    @PostMapping("create-short")
+    public String convertToShortUrl(@RequestBody UrlLongRequest request) {
+        return urlService.convertToShortUrl();
+    }
+
+    @GetMapping(value = "{shortUrl}")
+//    @Cacheable(value = "urls", key = "#shortUrl", sync = true)
+    public ResponseEntity<Void> getAndRedirect(@PathVariable String shortUrl) {
+        var url = urlService.getOriginalUrl(shortUrl);
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .location(URI.create(url))
+                .build();
+    }
+}
---
+++ url-shortener/src/main/java/com/example/urlshortener/service/UrlService.java
@@ -0,0 +1,89 @@
+package com.example.urlshortener.service;
+
+import org.springframework.stereotype.Service;
+
+//import java.beans.Encoder;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.*;
+
+@Service
+public class UrlService {
+
+    public String convertToShortUrl() {
+        return "sami";
+    }
+
+    public String getOriginalUrl(String shortUrl) {
+        String uuid = UUID.randomUUID().toString();
+
+        SecureRandom random = new SecureRandom();
+        byte bytes[] = new byte[7];
+        random.nextBytes(bytes);
+        Base64.Encoder encoder = Base64.getUrlEncoder().withoutPadding();
+        String token = encoder.encodeToString(bytes);
+        uuid = uuid.replace("-", "");
+//        for(int i = 0; i < token.length(); i++) {
+//            uuid = uuid.concat(String.valueOf((int) token.charAt(i)));
+//        }
+//        uuid = uuid.concat(token);
+        BigInteger intUUID = new BigInteger(uuid, 16);
+        System.out.println(uuid);
+        System.out.println(intUUID);
+        String output = "";
+        uuid = uuid.replace("-", "");
+//        BigInteger integerOutput = BigInteger.ONE;
+//        BigInteger multiply = BigInteger.ONE;
+//        for(int i = 0; i < uuid.length(); i++) {
+//            integerOutput = integerOutput.multiply(BigInteger.valueOf((int) uuid.charAt(i))).multiply(multiply);
+//            multiply = multiply.add(BigInteger.ONE);
+////            output += String.valueOf((int) uuid.charAt(i));
+//        }
+//        System.out.println("integerOutput: " + integerOutput);
+//        BigInteger integerOutput = new BigInteger(output);
+        String base64Encoding = Base64.getUrlEncoder().encodeToString(uuid.getBytes());
+        System.out.println("base64Encoding: " + base64Encoding);
+        List<BigInteger> abc = new ArrayList<>();
+        intUUID = BigInteger.valueOf(70);
+        while (intUUID != BigInteger.ZERO) {
+            BigInteger temp = intUUID.mod(BigInteger.valueOf(64));
+            intUUID = intUUID.divide(BigInteger.valueOf(64));
+            abc.add(temp);
+        }
+        Collections.reverse(abc);
+        System.out.println("list: " + abc);
+        shortUrl(abc);
+//        BigInteger uuidUpdated = BigInteger.ONE;
+//        BigInteger count = BigInteger.ONE;
+//        for (BigInteger list : abc) {
+//            uuidUpdated = uuidUpdated.multiply(list).multiply(count);
+//            count = count.add(BigInteger.ONE);
+//        }
+//        List<BigInteger> abcUpdated = new ArrayList<>();
+//        while (!uuidUpdated.divide(BigInteger.valueOf(62)).equals(BigInteger.ZERO)) {
+//            abcUpdated.add(uuidUpdated.mod(BigInteger.valueOf(62)));
+//            uuidUpdated = uuidUpdated.divide(BigInteger.valueOf(62));
+//        }
+//        System.out.println("abcUpdated: " + abcUpdated);
+        return "https://www.educative.io/courses/grokking-modern-system-design-interview-for-engineers-managers/evaluation-of-tinyurls-design";
+    }
+
+    private List<BigInteger> shortUrl(List<BigInteger> abc) {
+        while(abc.size() > 7) {
+            BigInteger uuidUpdated = BigInteger.ONE;
+            BigInteger count = BigInteger.ONE;
+            for (BigInteger list : abc) {
+                uuidUpdated = uuidUpdated.multiply(list).multiply(count);
+                count = count.add(BigInteger.ONE);
+            }
+            abc = new ArrayList<>();
+            while (!uuidUpdated.divide(BigInteger.valueOf(62)).equals(BigInteger.ZERO)) {
+                abc.add(uuidUpdated.mod(BigInteger.valueOf(62)));
+                uuidUpdated = uuidUpdated.divide(BigInteger.valueOf(62));
+            }
+            System.out.println("abcUpdated: " + abc);
+        }
+        return abc;
+    }
+}
